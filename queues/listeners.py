b4f19812a0dfc99e377f7f622b1a36f92b43a568
Kyryo <owenonions@gmail.com>
2022-09-14 18:45:01 +0200

    Added TODO comments

--- queues/listeners.py
+++ queues/listeners.py
@@ -6,9 +6,12 @@ from . import models, transmissions
 
 @receiver(post_save, sender=models.QueueItem)
 def queued_item_created(sender, instance, created, **kwargs):
-    """runs when an item is created"""
+    """runs when an item is created in the queue"""
 
     if created:
+        # [TODO] Add items to a "real" queue like Celery?
+        # [TODO] Check if routes are available. Start checking for the high weighted going down
+        # [TODO] If default routes are NOT available default to SMS route
         sms = transmissions.TextMessage("0998006237", "This is the message to send.")
         sms.connect_phone()
         sms.send_message()
